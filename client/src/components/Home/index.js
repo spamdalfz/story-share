3d5a96dba9442bac1d714142fb9c9a24508e86f1
spamdalfz <coreyvasser@gmail.com>
2023-07-13 18:01:35 -0700

    modified:   client/src/components/Home/index.js

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
+//import React, { useState, useEffect } from 'react';
+//import styled from 'styled-components';
 
 // eslint-disable-next-line no-unused-vars
 //const ButtonContainer = styled.div`
@@ -21,8 +21,8 @@ import styled from 'styled-components';
 //`;
 
 
-const Home = () => {
-    const [stories, setStories] = useState([]);
+//const Home = () => {
+    //const [stories, setStories] = useState([]);
 
     //const fetchStories = async () => {
     // try {
@@ -34,38 +34,38 @@ const Home = () => {
     //  }
     //  }
 
-    useEffect(() => {
+    //useEffect(() => {
         //      fetchStories();
-    }, []);
+    //}, //[]);
 
-    return (
-        <div className="bg-app-color min-h-screen">
-            <h1 className="text-8xl font-lobster text-app-color text-center py-4 bg-black">
-                Welcome to StoryShare!
-            </h1>
-            <p className="text-6xl font-bold text-center mb-4 font-marvel">
-                A place where you can share your stories and read stories from others!
-            </p>
-            <p className="text-6xl font-bold  text-center font-marvel">
-                Explore our prompts, read stories, or login/signup to start sharing your own.
-            </p>
-            <div className="container mx-auto py-8 px-4">
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                    {stories.map((story) => (
-                        <div
-                            key={story.id}
-                            className="bg-pink font-marvel font-bold rounded-lg shadow-md cursor-pointer p-4 hover:bg-yellow transition duration-300"
-                        >
-                            <h2 className="text-4xl hover:text-black text-black">
-                                {story.title}
-                            </h2>
-                            <p>{story.body.substring(0, 100)}...</p>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        </div>
-    );
-};
+   // return (
+       // <div className="bg-app-color min-h-screen">
+           // <h1 className="text-8xl font-lobster text-app-color text-center py-4 bg-black">
+               // Welcome to StoryShare!
+            //</h1>
+           // <p className="text-6xl font-bold text-center mb-4 font-marvel">
+              //  A place where you can share your stories and read stories from others!
+           // </p>
+          //  <p className="text-6xl font-bold  text-center font-marvel">
+              //  Explore our prompts, read stories, or login/signup to start sharing your own.
+           // </p>
+           // <div className="container mx-auto py-8 px-4">
+           //    // <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                    //{stories.map((story) => (
+                       // <div
+                       //     key={story.id}
+                      //      className="bg-pink font-marvel font-bold rounded-lg shadow-md cursor-pointer p-4 hover:bg-yellow transition duration-300"
+                     //   >
+                    //        <h2 className="text-4xl hover:text-black text-black">
+                         //       {story.title}
+                     //       </h2>
+                       //     <p>{story.body.substring(0, 100)}...</p>
+                     //   </div>
+                 //   ))}
+              //  </div>
+           // </div>
+       // </div>
+   // );
+//};
 
-export default Home;
+//export default Home;
